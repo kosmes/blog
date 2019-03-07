c41aca677ccf8fa865a9150f6895df79c3317108
kosmes <kosmes9779@gmail.com>
2019-03-07 19:40:30 +0900

    feat: Add PostModel for database

---
+++ src/main/java/com/jineyne/blog/model/PostModel.java
@@ -0,0 +1,50 @@
+package com.jineyne.blog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class PostModel {
+    @Id
+    @GeneratedValue
+    int id;
+    String title;
+    @Column(length = 100000000)
+    String content;
+    Date regDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+}
